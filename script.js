23a051563031a88bdd5ad311dc935726d93052fc
Hasanov Ali <alivh@code.local>
2023-12-16 14:12:38 +0400

    Feat

---
+++ script.js
@@ -0,0 +1,135 @@
+// console.log("AliCS402")
+
+
+
+// console.log(username1);
+// let username = "Nigger"
+// let username = "Metin"
+// // let username = "Ali"
+
+
+// username = "Ali"
+// console.log(username);
+//  username = "Criags"
+// //  username = "LLO"
+// //  console.log("2", username);
+
+
+
+
+
+
+
+
+// // let name = "Ali"
+// // console.log(name);
+// // // let lastname = "Gasanov"
+// // // console.log(lastname);
+
+// // // let age = "18"
+// // // console.log(age);
+
+// // let username = ('Ali')
+// // let userage = 18
+// // let isadmin = true
+// // let salary = ('10M')
+// // let date
+
+// // console.log(username);
+// // console.log(userage);
+// // console.log(isadmin);
+// // console.log(salary);
+// // console.log(date);
+
+
+
+// // let a = 8
+// // let b = 6
+// // console.log(a ^ b)
+
+// // let isAdmin = true
+// // let username = 'Al' 
+// // let age = (19)
+
+// // if (age >= 18){
+// //     console.log('Access_Granted');
+// // } else if (username == "Ali"){ 
+// //     console.log('Name_Yes');
+// // } else if (isAdmin) {
+// //     console.log("Admin_yes")
+// //  } else {
+// //     console.log('Error') 
+// //  } 
+
+//  let usrname = 'ali' 
+//  switch (usrname) {
+//     case "aa":
+//         console.log ("0");
+//         break;
+//  }
+//  switch (usrname) {
+//     case 'ab':
+//         console.log ("0");
+//         break;
+//  }
+//  switch (usrname) {
+//     case 'ali':
+//         console.log ("Age_True");
+//         break;
+//  }
+
+//  let age = (19)
+//  switch (age) {
+//     case 16 :
+//     console.log ("No_16");
+//         break;
+//  }
+//  switch (age) {
+//     case 19 :
+//     console.log ("Name_True");
+//         break;
+//  }
+ 
+//  let isAdmin = true
+//  switch (isAdmin) {
+//     case true :
+//         console.log ('Access_Granted');
+//         break;
+//  }
+// switch (isAdmin) {
+//  case NaN :
+//        console.log ('Deny');
+//        break;
+// }
+
+
+let isAdmin = NaN
+if (!isAdmin) {
+    console.log(isAdmin);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
