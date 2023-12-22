d4c84aeb0d3e32004e6d976149c2a78477c677c3
unknown <alivh@code.edu.az>
2023-12-22 18:01:07 +0400

    Feat

---
+++ script.js
@@ -0,0 +1,181 @@
+// // Task 1
+// console.log(12+8);
+
+// console.log(10/2);
+
+// let num1 = 20
+// let num2 = 17
+// console.log(20+17);
+
+// let name = 'Ali'
+// let surname= 'Hasanov'
+// let year= '2005'
+// let info = name + surname + year;
+// console.log(info);
+
+// let d1= 17
+// let d2 = 12
+// console.log(17%12);
+
+// cityName= 'Baku'
+// cityName= 'Gence'
+// console.log(cityName);
+
+// // Task 2
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+
+// console.log(c); //2
+// console.log(d); //1
+// console.log(b); //2
+
+// Task 3
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x); // 5
+
+// Task 4
+// console.log('test1', test); Error
+
+// {
+//     let test = "something"
+// //     console.log('test2', test);
+// // } test2 , something
+
+// console.log('test3', test); Error
+
+
+
+// console.log('test1', test);  // Error
+// {
+//     var test = "something"
+//     console.log('test2', test); //  test2 , something
+// }
+// console.log('test3', test); // Error
+
+
+// Task 5 
+
+//Find the type of all the following cases
+
+// let name = "Ulfat" string
+// let surname = Zakirli variable
+// let year = 2000 number
+// year = "2000" string
+// let city; undefined
+// let qualification = null; null
+// let obj = { name: 'ulfat' } object
+// let arr = ['a', 'b', 'c'] array
+
+// // Task 6 
+// const arr1 = [9, 1, 5, 8, 9, 9, 3, 22, 11, 12, 32, 8]
+
+// const sum = arr1.reduce((acc, num) => acc + num);
+// console.log(sum);
+
+// arr1.push(10, 88);
+// console.log(arr1);
+
+// arr1.splice(0, 2);
+// console.log(arr1);
+
+// arr1.unshift(0, 9, 11);
+// console.log(arr1);
+
+// arr1.shift();
+// console.log(arr1);
+
+// // Task7
+
+// const arr2 = ["Murad", "Anar", "Rufet", 'Anar', 'Gulshen', 'Nermin', "Ilkin", "Fuad", "Anar"]
+
+// // console.log(arr2.slice(2, -1));
+
+// arr2.push = ('Rovshan')
+//
+// console.log(arr2)
+
+// arr2.map(name => console.log(name));
+// console.log(name);
+
+// console.log(arr2.filter(name => name === 'Anar'));
+
+
+// arr2.forEach((name, index) => {
+//     if (name === 'Anar') {
+//         arr2[index] = 'Perviz';
+//     }
+// });
+// console.log(arr2);
+
+// console.log(arr2[arr2.length - 2]);
+
+// console.log(arr2.length);
+
+
+// // Task8
+
+// const arr3 = ["9", 10, 100, 11, 'true', 'undefined', true, null, 'something', false]
+
+// const numbers = arr3.filter(item => typeof item === 'number');
+// console.log(numbers);
+
+// const trueValues = arr3.filter(item => typeof item === 'boolean' && item === true);
+// console.log(trueValues);
+
+// const falseValues = arr3.filter(item => typeof item === 'boolean' && item === false);
+// console.log( falseValues);
+
+// const strings = arr3.filter(item => typeof item === 'string');
+// console.log(strings);
+
+
+// // Task8
+// const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// console.log(evenNumbers);
+
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+// console.log(oddNumbers);
+
+// Task9
+
+const arr4 = [10, "29", 10, 100, 11, 'true', false, 'undefined', true, null, 'something', false]
+
+// Task10
+
+
+
+// const obj2 = {
+//     name: "Ali",
+//     surname: 'Gasanov',
+//     age: 18,
+    
+//     email: 'ulfat@gmail.com',
+//     info: {
+//         gender: 'Male',
+//         loc: {
+//             city: "Baku",
+//             street: 'Nizami 22',
+//             education: {
+//                 uniname: "ADNSU",
+//             }
+//         }
+//     }
+// }
+
+// obj2.name = 'John'
+
+// console.log(obj2.info.gender);
+// console.log(obj2.info.loc.city);
+
+// console.log(obj2.info.loc.education.uniname);
+
+
+
+
+
