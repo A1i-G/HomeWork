e289d0bc671403391f828d7869bf187663634017
Hasanov Ali <alivh@code.local>
2023-12-23 14:37:38 +0400

    Feat

--- Array 0.3/script.js
+++ Array 0.3/script.js
@@ -1,73 +1,82 @@
-const info = [
-    {
-        id: 12,
-        name: 'Ali',
-        surname: 'Gasanov',
-        age: 18,
-        adress: {
-            street: "Breston",
-            City: 'NYC',
-            country: 'US',
+// // Class task
+// // 1.id,age,surname,name,(nested:address - street,city,country)
+// // 2.sum all index with reduce method
+// // 3.sort by age (asc)
+// // 4.sort by street (desc)
+// // 5.reverse
+// // 6.filter only name (where name length is equal 6 or higher)
+// // 7.find first object where age is greater than 25
+// // 8.create a new array which add 'myAge' word in front of age item
+// const info = [
+//     {
+//         id: 12,
+//         name: 'Ali',
+//         surname: 'Gasanov',
+//         age: 18,
+//         adress: {
+//             street: "Breston",
+//             City: 'NYC',
+//             country: 'US',
 
-        }
-    },
-    {
-        id: 13,
-        name: 'Vali',
-        surname: 'Demirov',
-        age: 17,
-        adress: {
-            street: "Bakers",
-            City: 'London',
-            country: 'UK',
-        },
-    },
-    {
-            id: 13,
-            name: 'Vali',
-            surname: 'Demirov',
-            age: 17,
-            adress: {
-                street: "Bakers",
-                City: 'London',
-                country: 'UK',
-            },
-        },
-        {
-            id: 13,
-            name: 'Vaeeli',
-            surname: 'Demirov',
-            age: 30,
-            adress: {
-                street: "Bakers",
-                City: 'London',
-                country: 'UK',
-            },
-        }
-    ]
+//         }
+//     },
+//     {
+//         id: 13,
+//         name: 'Vali',
+//         surname: 'Demirov',
+//         age: 17,
+//         adress: {
+//             street: "Bakers",
+//             City: 'London',
+//             country: 'UK',
+//         },
+//     },
+//     {
+//             id: 13,
+//             name: 'Vali',
+//             surname: 'Demirov',
+//             age: 17,
+//             adress: {
+//                 street: "Bakers",
+//                 City: 'London',
+//                 country: 'UK',
+//             },
+//         },
+//         {
+//             id: 13,
+//             name: 'Vaeeli',
+//             surname: 'Demirov',
+//             age: 30,
+//             adress: {
+//                 street: "Bakers",
+//                 City: 'London',
+//                 country: 'UK',
+//             },
+//         }
+//     ]
 
 
 
-let result1 = info.reduce((acc, curr, index) => acc + index, 0);
-console.log(result1);
+// let result1 = info.reduce((acc, curr, index) => acc + index, 0);
+// console.log(result1);
 
-const result2 = info.sort((a, b) => a.age - b.age)
-console.log(result2)
+// const result2 = info.sort((a, b) => a.age - b.age)
+// console.log(result2)
 
-const result3 = info.sort((a, b) => b.adress.street - a.adress.street)
-console.log(result3)
+// const result3 = info.sort((a, b) => b.adress.street - a.adress.street)
+// console.log(result3)
 
-const rev = info.reverse()
-console.log(rev);
+// const rev = info.reverse()
+// console.log(rev);
 
-const rev1 = info.filter((item, index) => item.age > 25)
-console.log(rev1);
+// const rev1 = info.filter((item, index) => item.age > 25)
+// console.log(rev1);
 
-const rev2 = info.find((item, index) => item.name.length >= 6)
-console.log(rev2);
+// const rev2 = info.find((item, index) => item.name.length >= 6)
+// console.log(rev2);
 
-const rev3 = info.map((item => item.name + ' Is My Name'))
-console.log(rev3);
+// const rev3 = info.map((item => item.name + ' Is My Name'))
+// console.log(rev3);
 
 
 // let user1 = 'Ulfat'
@@ -90,18 +99,20 @@ console.log(rev3);
 // console.log(rev8);
 
 
+let person = 'ulfat zakirli'
+const personArr1 = person.split('l')
+console.log(personArr1);
+const personArr2 = person.split(',')
+console.log(personArr2);
+const personArr3 = person.split(' ')
+console.log(personArr3);
+const personArr4 = person.split('')
+console.log(personArr4);
 
+const studentsArr = ['Yusif', 'Reshad', 'Nihad','Nezire','Metin', 'Heyder', 'Rafig','Eli']
 
-
-
-
-
-
-
-
-
-
-
+studentsnn = studentsArr.map(item => `name: ${item }`).join()
+console.log( studentsnn);
 
 
 
