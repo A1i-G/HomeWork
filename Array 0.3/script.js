b5ac2654bf43396ba94603083d387bcc5240f4fc
Hasanov Ali <alivh@code.local>
2023-12-23 13:58:15 +0400

    Feat

---
+++ Array 0.3/script.js
@@ -0,0 +1,113 @@
+const info = [
+    {
+        id: 12,
+        name: 'Ali',
+        surname: 'Gasanov',
+        age: 18,
+        adress: {
+            street: "Breston",
+            City: 'NYC',
+            country: 'US',
+
+        }
+    },
+    {
+        id: 13,
+        name: 'Vali',
+        surname: 'Demirov',
+        age: 17,
+        adress: {
+            street: "Bakers",
+            City: 'London',
+            country: 'UK',
+        },
+    },
+    {
+            id: 13,
+            name: 'Vali',
+            surname: 'Demirov',
+            age: 17,
+            adress: {
+                street: "Bakers",
+                City: 'London',
+                country: 'UK',
+            },
+        },
+        {
+            id: 13,
+            name: 'Vaeeli',
+            surname: 'Demirov',
+            age: 30,
+            adress: {
+                street: "Bakers",
+                City: 'London',
+                country: 'UK',
+            },
+        }
+    ]
+
+
+
+let result1 = info.reduce((acc, curr, index) => acc + index, 0);
+console.log(result1);
+
+const result2 = info.sort((a, b) => a.age - b.age)
+console.log(result2)
+
+const result3 = info.sort((a, b) => b.adress.street - a.adress.street)
+console.log(result3)
+
+const rev = info.reverse()
+console.log(rev);
+
+const rev1 = info.filter((item, index) => item.age > 25)
+console.log(rev1);
+
+const rev2 = info.find((item, index) => item.name.length >= 6)
+console.log(rev2);
+
+const rev3 = info.map((item => item.name + ' Is My Name'))
+console.log(rev3);
+
+
+// let user1 = 'Ulfat'
+
+// let rev6 = user1.slice(0,4).toLowerCase() + user1.slice(-1).toUpperCase() 
+// console.log(rev6);
+
+// let rev5 = user1.slice(1).toUpperCase()
+// console.log(rev5);
+
+// let rev4 =user1.slice(0,2).toLowerCase() + user1.slice(2,3).toUpperCase() + user1.slice(3).toLowerCase()
+// console.log(rev4);
+
+// let user = 'ulfat zakirli'
+
+// let rev7 =user.slice(0,1).toUpperCase() + user.slice(1,5).toLowerCase() + user.slice(5,7).toUpperCase() + user.slice(-6).toLowerCase()
+// console.log(rev7);
+
+// let rev8 = user.slice(0,6).toLowerCase() + user.slice(6).toUpperCase()
+// console.log(rev8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
