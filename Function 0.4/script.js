1d68f1ba530de3c8dd77af6ae061706ed5339f82
Hasanov Ali <alivh@code.local>
2023-12-24 14:07:45 +0400

    Feat

---
+++ Function 0.4/script.js
@@ -0,0 +1,128 @@
+// let age=23
+// {age > 22 
+// ? console.log('1') 
+//     : age == 20 
+//       ?console.log('2')
+//       :console.log('3');
+// // }
+
+// function sum23(){
+// return 7+6 
+// }
+// console.log(sum23());
+// user.map((user)=>sum += user)
+// console.log(sum);
+
+// console.log(sum);
+
+
+// const users = [23,24,35,43,54]
+
+// function getAvg(anyArray){
+//     let sum = 0
+//     anyArray.map((user)=>sum += user)
+//     return Math.round(sum / anyArray.length)
+// }
+// console.log(getAvg(users));
+
+// let age=17
+// {age > 22 
+// ? console.log('1') 
+//     : age == 20 
+//       ?console.log('2')
+//       :console.log('3');
+// }
+
+// {if (age>22) 
+//     console.log(1)
+// else if (age == 17) 
+// ( console.log(2)); 
+// }
+
+
+
+// const arrNum = [6,46,65,7,3,17] 
+// const studentsArr = ["Ali",'Blbt','Yusif ','Perviz',"Nigar"]
+
+// {if (studentsArr.filter(item => item.startsWith('A')))
+//     console.log(1)
+// else 
+// ( console.log(2));   
+// }
+// {
+//     if  (studentsArr.filter(item => item.endsWith('t')))
+// console.log(1)
+// else console.log(2);
+// }
+
+// function sumNum() {
+//     return 8+9
+// }
+// console.log(sumNum());
+
+// function multNum() {
+//     return 19*59*9
+// }
+// console.log(multNum());
+
+// function getAvg(arrNum){
+//     let sum = 0
+//     arrNum.map((arrNum)=>sum += arrNum)
+//      return Math.round(sum / arrNum.length)
+// }
+// console.log(getAvg(arrNum));
+
+// function getUsers() {
+//    const result = (studentsArr.filter(item => item.startsWith('N')))
+// return result
+// }
+// console.log(getUsers())
+
+// function getUsers2() {
+//     const result1 = (studentsArr.filter(item => item.length >3))
+//  return result1
+// }
+// console.log(getUsers2())
+
+// function getAv() {
+//     return arrNum.reduce((acc,curr)=> acc + curr,0)/ arrNum.length
+// }
+// console.log(getAv());
+
+
+// for (let i = 0; i < 21; i++) {
+//     if (i % 2 !== 0)
+//         console.log(i);
+// };
+// for (let i = 0; i < 21; i++) {
+//     if (i % 2 == 0)
+//         console.log(i);
+// };
+// const odd = i.filter(number => number % 2 !== 0);
+// console.log(odd);
+
+
+// let myName= "Ulfat"
+// for (let i = 0; i < myName.length; i ++) {
+// console.log(myName[i]);
+// };
+
+
+
+
+
+let studentsArr = ["Ali",'Blbt','Yusif ','Perviz',"Nigar"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
