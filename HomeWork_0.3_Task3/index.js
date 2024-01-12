f520cc7814e032e488ff0f755f16cf4c86d0bea5
unknown <alivh@code.edu.az>
2024-01-12 14:09:36 +0400

    Feat

---
+++ HomeWork_0.3_Task3/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+require('dotenv').config()
+const bodyParser = require('body-parser')
+require('colors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+let users = [
+    {
+        id: 1,
+        username: 'Ali'
+    },
+    {
+        id: 2,
+        username: 'Abbas'
+    },
+    {
+        id: 3,
+        username: 'Rafig'
+    },
+]
+
+
+app.get('/code/users', (req, res) => {
+    res.json(users)
+})
+
+app.delete('/code/users/:id', (req, res) => {
+    const { id } = req.params
+    users = users.filter((item) => item.id != id)
+    res.json('User deleted successfully!')
+})
+
+app.post('/code/users', (req, res) => {
+    const userBody = req.body
+    users.push(userBody)
+    res.send('User added successfully!')
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server is running: http://localhost:${PORT}`.green.bold))
